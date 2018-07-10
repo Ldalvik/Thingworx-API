13605a399a4a5e71c83f212f5d48854cc26faa1a
Julian Osborne <subaruforge@gmail.com>
2018-07-10 12:07:32 -0400

    Update Thingworx.java

--- Thingworx.java
+++ Thingworx.java
@@ -3,7 +3,7 @@ import org.json.simple.JSONObject;
 
 public class Thingworx {
     private String thing;
-
+    private String ENDPOINT = "http://pp-1804271345f2.portal.ptc.io:8080/Thingworx/Things/";
     public Thingworx(String thing){
         this.thing = thing;
     }
@@ -13,40 +13,40 @@ public class Thingworx {
         obj.put("description", description);
         obj.put("thingTemplateName", thingTemplateName);
         Utils.postRequest(
-                "http://pp-1804271345f2.portal.ptc.io:8080/Thingworx/Resources/EntityServices/Services/CreateThing",
+                ENDPOINT + "/Thingworx/Resources/EntityServices/Services/CreateThing",
                 obj.toJSONString()
         );
     }
 
     public Properties getPropertyValue(String property) {
-        return new Properties(Utils.getRequest("http://pp-1804271345f2.portal.ptc.io:8080/Thingworx/Things/" + thing + "/Properties/" + property), property);
+        return new Properties(Utils.getRequest(ENDPOINT + thing + "/Properties/" + property), property);
     }
 
-    public void addPropertyToThing(String name, String type){
+    public void addProperty(String name, String type){
         JSONObject obj = new JSONObject();
         obj.put("name", name);
         obj.put("type", type);
-        Utils.postRequest("http://pp-1804271345f2.portal.ptc.io:8080/Thingworx/Things/" + thing + "/Services/AddPropertyDefinition", obj.toJSONString());
+        Utils.postRequest(ENDPOINT + thing + "/Services/AddPropertyDefinition", obj.toJSONString());
     }
 
     public void updateProperty(String property, String value){
         JSONObject obj = new JSONObject();
         obj.put(property, value);
-        Utils.putRequest("http://pp-1804271345f2.portal.ptc.io:8080/Thingworx/Things/" + thing + "/Properties/*", obj.toJSONString());
+        Utils.putRequest(ENDPOINT + thing + "/Properties/*", obj.toJSONString());
     }
 
     public void updateProperty(String property, int value){
         JSONObject obj = new JSONObject();
         obj.put(property, value);
-        Utils.putRequest("http://pp-1804271345f2.portal.ptc.io:8080/Thingworx/Things/" + thing + "/Properties/*", obj.toJSONString());
+        Utils.putRequest(ENDPOINT + thing + "/Properties/*", obj.toJSONString());
     }
 
     public void restart(){
-        Utils.postRequest("http://pp-1804271345f2.portal.ptc.io:8080/Thingworx/Things/" + thing + "/Services/RestartThing");
+        Utils.postRequest(ENDPOINT + thing + "/Services/RestartThing");
     }
 
     public void enable(){
-        Utils.postRequest("http://pp-1804271345f2.portal.ptc.io:8080/Thingworx/Things/" + thing + "/Services/EnableThing");
+        Utils.postRequest(ENDPOINT + thing + "/Services/EnableThing");
     }
 }
 
